58a1537bad2b200bba3fc43ea192cdc522f9f5b7
KeiOnoda <onodakei1994@gmail.com>
2023-05-28 03:29:04 +0900

    Stripe実装追加 * サインアップ時にStripeにユーザー作成及びcreditテーブルにユーザー登録 * ログインユーザーの登録済みカード情報を取得するAPIを追加

---
+++ app/Models/domains/Stripe/MaskCardEntity.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models\domains\Stripe;
+
+use App\Models\domains\BaseDomain;
+
+class MaskCardEntity extends BaseDomain
+{
+    public function __construct(
+        public string    $cardId,
+        public string    $brand,
+        public string    $cvcCheck,
+        public int       $expMonth,
+        public int       $expYear,
+        public string    $last4,
+    ){}
+
+    public function getCardId(): string
+    {
+        return $this->cardId;
+    }
+
+    public function getBrand(): string
+    {
+        return $this->brand;
+    }
+
+    public function getCVCCheck(): string
+    {
+        return $this->cvcCheck;
+    }
+
+    public function getExpMonth(): int
+    {
+        return $this->expMonth;
+    }
+
+    public function getExpYeay(): int
+    {
+        return $this->expYear;
+    }
+
+    public function getLast4(): string
+    {
+        return $this->last4;
+    }
+}
